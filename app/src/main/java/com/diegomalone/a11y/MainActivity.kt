ebf4909cba225146e332407966ec817b2be61fb4
Diego Malone <diegomalone@gmail.com>
2019-05-11 21:38:28 -0300

    Adding reading traversal order manipulation

--- app/src/main/java/com/diegomalone/a11y/MainActivity.kt
+++ app/src/main/java/com/diegomalone/a11y/MainActivity.kt
@@ -2,11 +2,23 @@ package com.diegomalone.a11y
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ViewCompat.setAccessibilityDelegate(secondView, object : AccessibilityDelegateCompat() {
+            override fun onInitializeAccessibilityNodeInfo(host: View?, info: AccessibilityNodeInfoCompat?) {
+                info?.setTraversalAfter(thirdView)
+                super.onInitializeAccessibilityNodeInfo(host, info)
+            }
+        })
     }
 }
